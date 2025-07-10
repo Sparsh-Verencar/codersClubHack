2715f7d61fc89e0147d8e56558047ff43a87e989
Sparsh <verencarsparsh@gmail.com>
2025-07-10 21:48:06 +0530

    add shadcn buttons

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
 // components/Auth.js
+"use client"
 import { auth, googleProvider } from "./firebase";
 import { signInWithPopup, signOut } from "firebase/auth";
+import { Button } from "@/components/ui/button";
 
 export default function Auth() {
   const signInWithGoogle = async () => {
@@ -21,8 +23,8 @@ export default function Auth() {
 
   return (
     <div>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
-      <button onClick={logout}>Sign Out</button>
+      <Button variant={"outline"} onClick={signInWithGoogle}>Sign in with Google</Button>
+      <Button onClick={logout}>Sign in with Google</Button>
     </div>
   );
 }
