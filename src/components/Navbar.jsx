1fe491c989aa210b09679ff2aa77571bb00fda11
Sparsh <verencarsparsh@gmail.com>
2025-07-11 14:44:30 +0530

    landind page code added to this branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { ModeToggle } from '@/components/ui/mode-toggle'
 
 const Navbar = () => {
     return (
-        <div className='w-[80vw] h-[10vh] bg-amber-500 rounded-2xl text-amber-200'>
+        <div className='w-[80vw] h-[10vh] bg-amber-500 rounded-2xl text-amber-200 m-5 flex items-center justify-around'>
             navbar
             <ModeToggle />
         </div>
